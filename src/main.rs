62718bb388c52f81d94b51fa6f2ac9de3db32e09
golgepapaz <hsolter@gmail.com>
2020-07-16 17:40:12 +0300

    continue

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,13 @@ const ASPECT_RATIO: f64 =  16.0 / 9.0;
 const IMAGE_WIDTH: i32 = 384;
 const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
 
+let viewport_height = 2.0;
+let viewport_width = ASPECT_RATIO * viewport_height;
+let focal_length = 1.0;
 
+let origin = Vec3::new(0.,0.,0.);
+let horizontal = Vec3::new(viewport_width, 0., 0.);
+let vertical = Vec3::new(0., viewport_height, 0.);
+let lower_left = origin - horizontal/2. - vertical/2. - Vec3::new(0.,0.,focal_length);
 
 }
